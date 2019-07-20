00fba586ed2d19467aaa7823646850d1e18a3297
Ajay Gupta <ajaygupta.gupta636@gmail.com>
2019-07-20 21:18:23 +0530

    Changes made and ready to work on back-end

---
+++ BuildMyPc/src/java/com/GRInfotech/BuildMyPC/BuildMyPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GRInfotech.BuildMyPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildMyPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
