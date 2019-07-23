a0869ed9dc7540afc4136a9898c88598cd6dc73a
TheGUGLY <ajaygupta.gupta636@gmail.com>
2019-07-23 16:50:23 +0530

    update repos

--- BuildMyPc/src/main/java/com/app/controller/AdminController.java
+++ BuildMyPc/src/main/java/com/app/controller/AdminController.java
@@ -1,14 +1,32 @@
 package com.app.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin(origins = "http://localhost:4200",allowedHeaders="*")
+import com.app.dao.IItemRepo;
+import com.app.dao.ILoginRepo;
+import com.app.dao.IOrderRepo;
+import com.app.dao.IProductRepo;
+import com.app.dao.IUserRepo;
+
+
+//@CrossOrigin(origins = "http://localhost:4200",allowedHeaders="*")
 @RequestMapping("/Admin")
+@RestController
 public class AdminController {
+	
+	@Autowired
+	private IItemRepo item;
+	@Autowired
+	private ILoginRepo login;
+	@Autowired
+	private IOrderRepo order;
+	@Autowired
+	private IUserRepo user;
+	@Autowired
+	private IProductRepo product;
 	
 	@GetMapping
 	public String Testme(){
